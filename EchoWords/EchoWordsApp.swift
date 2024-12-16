07f75720570cdc090e4443e757d21c52fedee11d
WajeehaFatimaHassni <hassniwajeeha@gmail.com>
2024-12-16 15:02:05 +0100

    Initial Commit

---
+++ EchoWords/EchoWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoWordsApp.swift
+//  EchoWords
+//
+//  Created by Wajeeha Fatima Hassni on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct EchoWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
